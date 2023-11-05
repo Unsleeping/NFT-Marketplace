97cc6f78acd21bda6bd459930ef76c4dcbff56a8
Alexander Mandrov <otvet-1995@mail.ru>
2023-11-05 21:43:49 +0200

    minor fix

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -7,7 +7,9 @@ interface ButtonProps {
 const Button = ({ title, classStyles, onClick }: ButtonProps) => (
   <button
     type="button"
-    className={`nft-gradient text-sm minlg:text-lg py-2 px-6 minlg:px-8 font-poppins font-semibold text-white ${classStyles}`}
+    className={`nft-gradient text-sm minlg:text-lg py-2 px-6 minlg:px-8 font-poppins font-semibold text-white ${
+      classStyles ?? ""
+    }`}
     onClick={onClick}
   >
     {title}
